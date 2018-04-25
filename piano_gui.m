f7dd63cbf0c60ef4ea211a417880fc93b31c5b7e
Erica Peng <etpeng9296@gmail.com>
2018-04-25 00:07:20 -0400

    Updated guitar GUI

--- piano_gui.m
+++ piano_gui.m
@@ -23,7 +23,7 @@ function varargout = piano_gui(varargin)
 
 % Edit the above text to modify the response to help piano_gui
 
-% Last Modified by GUIDE v2.5 05-Apr-2018 11:49:56
+% Last Modified by GUIDE v2.5 24-Apr-2018 22:54:20
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -54,15 +54,22 @@ function piano_gui_OpeningFcn(hObject, eventdata, handles, varargin)
 % varargin   command line arguments to piano_gui (see VARARGIN)
 
 % Global variables
-global notes numNotes;
+global notes numNotes instr;
 numNotes = 37;
+instr = 1; % Default to start on piano
 
 % INITIALIZATION GOES HERE
 for i = 1:numNotes
-    filename = strcat('notes/note_',num2str(i-1),'.mp4');
+    filename = strcat('notes/piano/note_',num2str(i-1),'.mp4');
     [y, Fs] = audioread(filename);
-    notes(i).y = y;
-    notes(i).Fs = Fs;
+    notes(1,i).y = y;
+    notes(1,i).Fs = Fs;
+end
+for i = 1:numNotes
+    filename = strcat('notes/guitar/note_',num2str(i-1),'.wav');
+    [y, Fs] = audioread(filename);
+    notes(2,i).y = y;
+    notes(2,i).Fs = Fs;
 end
 notes
 
@@ -92,8 +99,8 @@ function pushbutton1_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton1 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-global notes;
-sound(notes(13).y,notes(13).Fs);
+global notes instr;
+sound(notes(instr, 13).y,notes(instr, 13).Fs);
 
 
 
@@ -102,8 +109,8 @@ function pushbutton2_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton2 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-global notes;
-sound(notes(15).y,notes(15).Fs);
+global notes instr;
+sound(notes(instr, 15).y,notes(instr, 15).Fs);
 
 
 % --- Executes on button press in C#2.
@@ -111,8 +118,8 @@ function pushbutton3_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton3 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-global notes;
-sound(notes(14).y,notes(14).Fs);
+global notes instr;
+sound(notes(instr, 14).y,notes(instr, 14).Fs);
 
 
 % --- Executes on button press in E2.
@@ -120,8 +127,8 @@ function pushbutton4_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton4 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-global notes;
-sound(notes(17).y,notes(17).Fs);
+global notes instr;
+sound(notes(instr, 17).y,notes(instr, 17).Fs);
 
 
 % --- Executes on button press in D#2.
@@ -129,8 +136,8 @@ function pushbutton6_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton6 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-global notes;
-sound(notes(16).y,notes(16).Fs);
+global notes instr;
+sound(notes(instr, 16).y,notes(instr, 16).Fs);
 
 
 % --- Executes on button press in F2.
@@ -138,8 +145,8 @@ function pushbutton7_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton7 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-global notes;
-sound(notes(18).y,notes(18).Fs);
+global notes instr;
+sound(notes(instr, 18).y,notes(instr, 18).Fs);
 
 
 % --- Executes on button press in G2.
@@ -147,8 +154,8 @@ function pushbutton8_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton8 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-global notes;
-sound(notes(20).y,notes(20).Fs);
+global notes instr;
+sound(notes(instr, 20).y,notes(instr, 20).Fs);
 
 
 % --- Executes on button press in A.
@@ -156,8 +163,8 @@ function pushbutton9_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton9 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-global notes;
-sound(notes(22).y,notes(22).Fs);
+global notes instr;
+sound(notes(instr, 22).y,notes(instr, 22).Fs);
 
 
 % --- Executes on button press in B2.
@@ -165,8 +172,8 @@ function pushbutton10_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton10 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-global notes;
-sound(notes(24).y,notes(24).Fs);
+global notes instr;
+sound(notes(instr, 24).y,notes(instr, 24).Fs);
 
 
 % --- Executes on button press in F#2.
@@ -174,8 +181,8 @@ function pushbutton11_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton11 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-global notes;
-sound(notes(19).y,notes(19).Fs);
+global notes instr;
+sound(notes(instr, 19).y,notes(instr, 19).Fs);
 
 
 % --- Executes on button press in G#2.
@@ -183,8 +190,8 @@ function pushbutton12_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton12 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-global notes;
-sound(notes(21).y,notes(21).Fs);
+global notes instr;
+sound(notes(instr, 21).y,notes(instr, 21).Fs);
 
 
 % --- Executes on button press in A#2.
@@ -192,8 +199,8 @@ function pushbutton13_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton13 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-global notes;
-sound(notes(23).y,notes(23).Fs);
+global notes instr;
+sound(notes(instr, 23).y,notes(instr, 23).Fs);
 
 
 % --- Executes on button press in C3.
@@ -201,8 +208,8 @@ function pushbutton14_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton14 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-global notes;
-sound(notes(25).y,notes(25).Fs);
+global notes instr;
+sound(notes(instr, 25).y,notes(instr, 25).Fs);
 
 
 % --- Executes on button press in D3.
@@ -210,8 +217,8 @@ function pushbutton15_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton15 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-global notes;
-sound(notes(27).y,notes(27).Fs);
+global notes instr;
+sound(notes(instr, 27).y,notes(instr, 27).Fs);
 
 
 % --- Executes on button press in C#3.
@@ -219,8 +226,8 @@ function pushbutton16_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton16 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-global notes;
-sound(notes(26).y,notes(26).Fs);
+global notes instr;
+sound(notes(instr, 26).y,notes(instr, 26).Fs);
 
 
 % --- Executes on button press in D#3.
@@ -228,8 +235,8 @@ function pushbutton17_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton17 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-global notes;
-sound(notes(29).y,notes(29).Fs);
+global notes instr;
+sound(notes(instr, 29).y,notes(instr, 29).Fs);
 
 
 % --- Executes on button press in D#3.
@@ -237,8 +244,8 @@ function pushbutton18_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton18 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-global notes;
-sound(notes(28).y,notes(28).Fs);
+global notes instr;
+sound(notes(instr, 28).y,notes(instr, 28).Fs);
 
 
 % --- Executes on button press in F3.
@@ -246,8 +253,8 @@ function pushbutton19_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton19 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-global notes;
-sound(notes(30).y,notes(30).Fs);
+global notes instr;
+sound(notes(instr, 30).y,notes(instr, 30).Fs);
 
 
 % --- Executes on button press in G3.
@@ -255,8 +262,8 @@ function pushbutton20_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton20 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-global notes;
-sound(notes(32).y,notes(32).Fs);
+global notes instr;
+sound(notes(instr, 32).y,notes(instr, 32).Fs);
 
 
 % --- Executes on button press in A3.
@@ -264,8 +271,8 @@ function pushbutton21_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton21 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-global notes;
-sound(notes(34).y,notes(34).Fs);
+global notes instr;
+sound(notes(instr, 34).y,notes(instr, 34).Fs);
 
 
 % --- Executes on button press in B3.
@@ -273,8 +280,8 @@ function pushbutton22_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton22 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-global notes;
-sound(notes(36).y,notes(36).Fs);
+global notes instr;
+sound(notes(instr, 36).y,notes(instr, 36).Fs);
 
 
 % --- Executes on button press in F#3.
@@ -282,8 +289,8 @@ function pushbutton23_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton23 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-global notes;
-sound(notes(31).y,notes(31).Fs);
+global notes instr;
+sound(notes(instr, 31).y,notes(instr, 31).Fs);
 
 
 % --- Executes on button press in G#3.
@@ -291,8 +298,8 @@ function pushbutton24_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton24 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-global notes;
-sound(notes(33).y,notes(33).Fs);
+global notes instr;
+sound(notes(instr, 33).y,notes(instr, 33).Fs);
 
 
 % --- Executes on button press in A#3.
@@ -300,8 +307,8 @@ function pushbutton25_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton25 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-global notes;
-sound(notes(35).y,notes(35).Fs);
+global notes instr;
+sound(notes(instr, 35).y,notes(instr, 35).Fs);
 
 
 % --- Executes on button press in C1.
@@ -309,8 +316,8 @@ function pushbutton38_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton38 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-global notes;
-sound(notes(1).y,notes(1).Fs);
+global notes instr;
+sound(notes(instr, 1).y,notes(instr, 1).Fs);
 
 
 % --- Executes on button press in D2.
@@ -318,8 +325,8 @@ function pushbutton39_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton39 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-global notes;
-sound(notes(3).y,notes(3).Fs);
+global notes instr;
+sound(notes(instr, 3).y,notes(instr, 3).Fs);
 
 
 % --- Executes on button press in C#1.
@@ -327,8 +334,8 @@ function pushbutton40_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton40 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-global notes;
-sound(notes(2).y,notes(2).Fs);
+global notes instr;
+sound(notes(instr, 2).y,notes(instr, 2).Fs);
 
 
 
@@ -337,8 +344,8 @@ function pushbutton41_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton41 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-global notes;
-sound(notes(5).y,notes(5).Fs);
+global notes instr;
+sound(notes(instr, 5).y,notes(instr, 5).Fs);
 
 
 % --- Executes on button press in D#1.
@@ -346,8 +353,8 @@ function pushbutton42_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton42 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-global notes;
-sound(notes(4).y,notes(4).Fs);
+global notes instr;
+sound(notes(instr, 4).y,notes(instr, 4).Fs);
 
 
 % --- Executes on button press in F1.
@@ -355,8 +362,8 @@ function pushbutton43_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton43 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-global notes;
-sound(notes(6).y,notes(6).Fs);
+global notes instr;
+sound(notes(instr, 6).y,notes(instr, 6).Fs);
 
 
 % --- Executes on button press in G1.
@@ -364,8 +371,8 @@ function pushbutton44_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton44 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-global notes;
-sound(notes(8).y,notes(8).Fs);
+global notes instr;
+sound(notes(instr, 8).y,notes(instr, 8).Fs);
 
 
 % --- Executes on button press in A1.
@@ -373,8 +380,8 @@ function pushbutton45_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton45 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-global notes;
-sound(notes(10).y,notes(10).Fs);
+global notes instr;
+sound(notes(instr, 10).y,notes(instr, 10).Fs);
 
 
 % --- Executes on button press in B1.
@@ -382,8 +389,8 @@ function pushbutton46_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton46 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-global notes;
-sound(notes(12).y,notes(12).Fs);
+global notes instr;
+sound(notes(instr, 12).y,notes(instr, 12).Fs);
 
 
 % --- Executes on button press in F#1.
@@ -391,8 +398,8 @@ function pushbutton47_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton47 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-global notes;
-sound(notes(7).y,notes(7).Fs);
+global notes instr;
+sound(notes(instr, 7).y,notes(instr, 7).Fs);
 
 
 % --- Executes on button press in G#1.
@@ -400,8 +407,8 @@ function pushbutton48_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton48 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-global notes;
-sound(notes(9).y,notes(9).Fs);
+global notes instr;
+sound(notes(instr, 9).y,notes(instr, 9).Fs);
 
 
 % --- Executes on button press in A#1.
@@ -409,5 +416,30 @@ function pushbutton49_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton49 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-global notes;
-sound(notes(11).y,notes(11).Fs);
+global notes instr;
+sound(notes(instr, 11).y,notes(instr, 11).Fs);
+
+
+% --- Executes on selection change in popupmenu1.
+function popupmenu1_Callback(hObject, eventdata, handles)
+% hObject    handle to popupmenu1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global instr;
+instr = handles.popupmenu1.Value;
+
+% Hints: contents = cellstr(get(hObject,'String')) returns popupmenu1 contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from popupmenu1
+
+
+% --- Executes during object creation, after setting all properties.
+function popupmenu1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to popupmenu1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
